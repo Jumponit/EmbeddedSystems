924c5ecd890684f1f9ca5b54d95b8b582f46792d
Joyce <joycemj@appd.appstate.edu>
2016-05-07 15:53:11 -0400

    Accepts 5 digits, all io commands work

--- System/System/main.c
+++ System/System/main.c
@@ -15,6 +15,8 @@
 #include "acx.h"
 #include "DS18B20.h"
 
+#define light_bulbs PB5 //Digital pin 11
+#define fans PB4 //Digital pin 11
 /*
  * The most-recently measured temperature in degrees Celsius
  */
@@ -34,7 +36,7 @@ volatile char service_mode;
 /*
  * Number of milliseconds to wait between temperature sampling.
  */
-volatile char sample_rate = 250;
+volatile unsigned int sample_rate = 5000;
 
 /*
  * The format string used to generate output
@@ -61,12 +63,13 @@ volatile int timeout = 60;
  */
 void io_controller(void) {
 	Serial_open(0,19200,SERIAL_8N1);
-	int command_len = 5;
-	int opcode_len = 3;
+	int command_len = 8;
+	int opcode_len = 2;
+	int operand_len = 6;
 	char command[command_len];
 	char opcode[opcode_len];
-	char operand[3];
-	//char message[64];
+	char operand[operand_len];
+	char message[64];
 	char * str;
 	char * formatStr;
 	char * format = "Last temp: 0x%x raw hex\n\r";
@@ -75,6 +78,17 @@ void io_controller(void) {
 		if(Serial_read_string(0,command,command_len)) {
 			opcode[0] = command[0];
 			opcode[1] = command[1];
+			for (int i = 0; i < operand_len; i++)
+			{
+				if (command[i+2] != 0x00)
+				{
+					operand[i] = command[i+2];
+				}
+				else
+				{
+					operand[i] = 0x00;
+				}
+			}
 			/************************************************************************/
 			/* Mode-selection commands                                              */
 			/************************************************************************/
@@ -100,12 +114,6 @@ void io_controller(void) {
 				/* Mode-specific commands                                               */
 				/************************************************************************/
 				if(service_mode) {
-					operand[0] = command[2];
-					operand[1] = 0x00;
-					if (command[3] != 0x00)
-					{
-						operand[1] = command[3];
-					}
 					//do service mode things
 					if (!strcmp(opcode, "GT")) {
 						//Get temperature
@@ -114,61 +122,44 @@ void io_controller(void) {
 							//this is equivalent to (9/5)*C + 32
 							fmt_temp = ((fmt_temp + (fmt_temp << 3))+160)/5;
 						}
-						str="";
-						if (sprintf(str, format, fmt_temp) < 0) {
-							str = "Formatting Error\n\r";
-						}
-						Serial_write_string(0, str, strlen(str));
+						sprintf((char *) message, format, fmt_temp);
+						Serial_write_string(0, (char *) message, strlen((char *) message));
 					} 
 					else if (!strcmp(opcode, "OV")) {
 						over_temp = atoi(operand);
 						formatStr = "Over-temperature set to %d degrees Celsius\n\r";
-						str="";
-						if (sprintf(str,formatStr,over_temp) < 0) {
-							str = "Formatting Error\n\r";
-						}
-						Serial_write_string(0, str, strlen(str));
+						sprintf((char *) message,formatStr,over_temp);
+						Serial_write_string(0, (char *) message, strlen((char *) message));
 					} 
 					else if (!strcmp(opcode, "SO")) {
 						timeout = operand[0] * 60;
 						formatStr = "Timeout set to %d seconds\n\r";
-						str="";
-						if (sprintf(str,formatStr,timeout) < 0) {
-							str = "Formatting Error\n\r";
-						}
-						Serial_write_string(0, str, strlen(str));
+						sprintf((char *) message,formatStr,timeout);
+						Serial_write_string(0, (char *) message, strlen((char *) message));
 					}
 					else {
 						str = "Unrecognized command\n\r";
 						Serial_write_string(0,str,strlen(str));
 					}
 				} else {
-					operand[0] = command[2];
-					operand[1] = 0x00;
-					if (command[3] != 0x00)
-					{
-						operand[1] = command[3];
-					}
 					//do operating mode things
 					if (!strcmp(opcode, "ST")) {
 						//set temperature
 						target_temp = atoi(operand);
 						if (target_temp < 0 || target_temp > 125) {
 							str = "Invalid temperature selection. Sucks to suck.\n\r";
+							Serial_write_string(0,str,strlen(str));
 						} else {
 							formatStr = "Set target temperature to %d degrees Celsius\n\r";
-							str="";
-							if (sprintf(str,formatStr,target_temp) < 0) {
-								str = "Formatting Error\n\r";
-							}
-						}
-						Serial_write_string(0,str,strlen(str));
+							sprintf((char *) message,formatStr,target_temp);
+							Serial_write_string(0, (char *) message, strlen((char *) message));
+						}	
 					} else if (!strcmp(opcode, "SR")) {
 						//set sample rate
-						sample_rate = operand[0];
-						format="Last temp: %x raw hex";
-						str = "Set format to Celsius Hexadecimal\n\r";
-						Serial_write_string(0,str,strlen(str));
+						sample_rate = atoi(operand);
+						formatStr = "Set sample rate to %u\n\r";
+						sprintf((char *) message,formatStr,sample_rate);
+						Serial_write_string(0, (char *) message, strlen((char *) message));
 					} else if (!strcmp(opcode, "SD")) {
 						//set display format
 						switch (operand[0]) {
@@ -215,12 +206,12 @@ void io_controller(void) {
  */
 void box_controller(void) {
 	//TODO: blink LED
-	DDRB |= 0x1 << PB4;
+	DDRB |= (0x1 << light_bulbs) | (0x1 << fans);
 	//PORTB |= 0x1 << PB4;
 	while(1) {
 		x_delay(sample_rate);
 		//_delay_ms(1000);
-		PORTB ^= 0x10;
+		PORTB ^= (0x1 << light_bulbs) | (0x1 << fans);
 		//x_yield();
 	}
 }
